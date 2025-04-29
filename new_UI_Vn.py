eeb5c5e94726fd571e5bd4d674a1e712b03a4d51
sreshta24 <sreshta.v@bridgehorn.com>
2025-04-29 16:37:02 +0530

    Update new_UI_Vn.py

--- new_UI_Vn.py
+++ new_UI_Vn.py
@@ -606,7 +606,7 @@ with st.sidebar:
         st.session_state.chat_mode = chat_mode
         st.rerun()
 
-    st.image("static/logo.jpg", use_container_width=True)
+    st.image("logo.jpg", use_container_width=True)
 
     cols = st.columns(3)  # Create 3 columns in one row
 
