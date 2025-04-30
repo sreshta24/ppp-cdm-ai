840ddc4cf09d606b19f916792137de4127a27baf
sreshta24 <sreshta.v@bridgehorn.com>
2025-04-30 15:38:00 +0530

    Update new_UI_Vn.py

--- new_UI_Vn.py
+++ new_UI_Vn.py
@@ -991,8 +991,6 @@ def get_llm_summary(user_prompt: str, df: pd.DataFrame) -> str:
         response = client.chat.completions.create(
                 model="o3-mini",  # Or "gpt-4" / "gpt-4-0125-preview"
                 messages=[{"role": "user", "content": prompt}],
-                max_completion_tokens=1024,
-                temperature=0.7
             )
         return response.choices[0].message.content
     except Exception as e:
@@ -1036,8 +1034,7 @@ Use clear language and help Cortex Analyst build the most accurate query.
         response = client.chat.completions.create(
             model="o3-mini",  # Or "gpt-4" / "gpt-4-0125-preview"
             messages=[{"role": "user", "content": new_prompt}],
-            max_completion_tokens=1024,
-            temperature=0.7
+            
         )
         return response.choices[0].message.content
     except Exception as e:
